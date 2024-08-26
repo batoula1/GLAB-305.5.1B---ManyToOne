507ba0970a201ac0db09227ff3143b244556da4e
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-26 02:47:25 -0400

    almostDone

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,54 +1,83 @@
 package model;
 
+
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Table
 public class Teacher implements Serializable {
     @Serial
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUId = 1L;
     @Id
-    @GeneratedValue( strategy=GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int teacherId;
     private String salary;
     private String TeacherName;
 
-    @ManyToOne
-    private Department department;
-    public Teacher(int teacherId, String salary, String teacherName) {
-        super();
-        this.teacherId = teacherId;
+    // @OneToOne(cascade = CascadeType.ALL)
+    // private Address address;
+
+    @ManyToMany(targetEntity = Cohort.class)
+    private Set<Cohort> cohort;
+
+    public void setCohort(Set<Cohort> cohort) {
+        this.cohort = cohort;
+    }
+
+    // public Address getAddress() {
+//        return address;
+//    }
+
+    // public void setAddress(Address address) {
+//        this.address = address;
+//    }
+
+    public Teacher(String salary, String teacherName, Set<Cohort> cohort) {
         this.salary = salary;
-        TeacherName = teacherName; }
-    public Teacher() {}
-
-    public Teacher(String s, String mHaseeb, Department dept1) {
+        TeacherName = teacherName;
     }
 
-    public Department getDep() {
-        return department; }
-    public void setDep(Department department) {
-        this.department = department;
+    public  Teacher(String salary, String teacherName){
+        super();
+        this.salary = salary;
+        this.TeacherName = teacherName;
     }
+
+    public Teacher() {
+    }
+
+    public Teacher(String salary, String teacherName, Department deptartment){
+        this.salary =salary;
+        this.TeacherName = teacherName;
+    }
+
     public int getTeacherId() {
         return teacherId;
     }
+
     public void setTeacherId(int teacherId) {
         this.teacherId = teacherId;
     }
+
     public String getSalary() {
         return salary;
     }
+
     public void setSalary(String salary) {
         this.salary = salary;
     }
+
     public String getTeacherName() {
         return TeacherName;
     }
+
     public void setTeacherName(String teacherName) {
-        TeacherName = teacherName; }
+        TeacherName = teacherName;
+    }
+
 }
 
